5eeeca2cefa300231dd0b7fe47ace8c59b1300e9
Sanekakf <sashasaltikov2007@gmail.com>
2021-07-28 21:22:30 +0300

    Update sanekakf.py
    Co-Authored-By: Fredjff <88098944+fredjff@users.noreply.github.com>

--- sanekakf.py
+++ sanekakf.py
@@ -36,21 +36,24 @@ class Player(pygame.sprite.Sprite):
         #выступы-заскоки
         if self.rect.right > width:
             self.rect.right = width
+
         if self.rect.left < 0:
             self.rect.left = 0
+
         if self.rect.top > height:
-            self.rect.top = height
+            self.rect.top = 0
+            
         if self.rect.bottom < 0:
-            self.rect.bottom = 0
+            self.rect.bottom = height
         #движение
         if key[K_LEFT]:
             self.speedx = -8
         if key[K_RIGHT]:
             self.speedx = 8
         if key[K_UP]:
-            self.speedy = 8
-        if key[K_DOWN]: 
             self.speedy = -8
+        if key[K_DOWN]: 
+            self.speedy = 8
         self.rect.x += self.speedx
         self.rect.y += self.speedy
 #запуск игры
