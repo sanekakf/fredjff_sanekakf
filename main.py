d1047d7c5957aa2b907755b9301ac704dee01e0c
Fredjff <fedyalol2008@yandex.ru>
2021-07-28 20:28:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import pygame
 import random 
 
-WIDTH = 360 #ширина игрового окна 
+WIDTH = 480 #ширина игрового окна 
 HEIGHT = 480 #высота игрового окна
 FPS = 60 #частота кадров в секунду 
 
@@ -13,7 +13,20 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255 )
+BLUE = (0, 0, 255)
+PURPLE = (160, 0, 200)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,50))
+        self.image.fill(PURPLE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 
 
@@ -24,6 +37,7 @@ pygame.mixer.init() #для звука
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My game")
 clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
 
 #цикл игры
 running = True 
@@ -35,8 +49,16 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
-   
+
+# обновление 
+all_sprites.update()
+
+
+
+
+
 screen.fill(BLACK)
+all_sprites.draw(screen)
 # после отрисовки всего, переворачиваем экран 
 pygame.display.flip()
 
