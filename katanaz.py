713a6e88698a404447bdb062198be7b990a01f58
Sanekakf <sashasaltikov2007@gmail.com>
2021-07-29 17:13:44 +0300

    Update katanaz.py

--- katanaz.py
+++ katanaz.py
@@ -4,7 +4,7 @@ app = Ursina()
 from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 player = PlatformerController2d(y=1, z=.01, scale_y=1, max_jumps=2)
 
-ground = Entity(model='quad', scale_x=10, collider='box', color=color.black)
+# ground = Entity(model='quad', scale_x=10, collider='box', color=color.black)
 
 quad = load_model('quad')
 print('--------------', quad)
